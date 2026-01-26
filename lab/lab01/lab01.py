948a9f55ffe64b5f4e958574f90693bc16af0132
mira wagner <mirawagner@berkeley.edu>
2026-01-26 18:16:04 +0000

    Deploy website Mon Jan 26 18:16:01 UTC 2026

---
+++ lab/lab01/lab01.py
@@ -0,0 +1,44 @@
+def harmonic_mean(x, y):
+    """Return the harmonic mean of x and y.
+
+    >>> harmonic_mean(2, 6)
+    3.0
+    >>> harmonic_mean(1, 1)
+    1.0
+    >>> harmonic_mean(2.5, 7.5)
+    3.75
+
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def digit(n, k):
+    """Return the k-th digit from the right of n for positive integers n and k.
+
+    >>> digit(3579, 2)
+    5
+    >>> digit(3579, 0)
+    9
+    >>> digit(3579, 10)
+    0
+    """
+    return ____
+
+
+def middle(a, b, c):
+    """Return the number among a, b, and c that is not the smallest or largest.
+    Assume a, b, and c are all different numbers.
+
+    >>> middle(3, 5, 4)
+    4
+    >>> middle(30, 5, 4)
+    5
+    >>> middle(3, 5, 40)
+    5
+    >>> middle(3, 5, 40)
+    5
+    >>> middle(30, 5, 40)
+    30
+    """
+    return ____
+
