1b1d8eebcaf2ae6ab445c51e4511c2fe70234567
mira wagner <mirawagner@berkeley.edu>
2026-01-27 17:19:42 +0000

    Deploy website Tue Jan 27 17:19:38 UTC 2026

---
+++ hw/hw01/hw01.py
@@ -0,0 +1,56 @@
+from math import sqrt
+
+def square(x):
+    return x * x
+
+def distance(x1, y1, x2, y2):
+    """Calculates the Euclidian distance between two points (x1, y1) and (x2, y2)
+
+    >>> distance(1, 1, 1, 2)
+    1.0
+    >>> distance(1, 3, 1, 1)
+    2.0
+    >>> distance(1, 2, 3, 4)
+    2.8284271247461903
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def two_of_three(i, j, k):
+    """Return m*m + n*n, where m and n are the two smallest members of the
+    positive numbers i, j, and k.
+
+    >>> two_of_three(1, 2, 3)
+    5
+    >>> two_of_three(5, 3, 1)
+    10
+    >>> two_of_three(10, 2, 8)
+    68
+    >>> two_of_three(5, 5, 5)
+    50
+    """
+    return _____
+
+def two_of_three_syntax_check():
+    """Check that your two_of_three code consists of nothing but a return statement.
+
+    >>> # You aren't expected to understand the code of this test.
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(two_of_three)).body[0].body]
+    ['Expr', 'Return']
+    """
+    # You don't need to edit this function. It's just here to check your work.
+
+
+def largest_factor(n):
+    """Return the largest factor of n that is smaller than n.
+
+    >>> largest_factor(15) # factors are 1, 3, 5
+    5
+    >>> largest_factor(80) # factors are 1, 2, 4, 5, 8, 10, 16, 20, 40
+    40
+    >>> largest_factor(13) # factors are 1, 13
+    1
+    """
+    "*** YOUR CODE HERE ***"
+
