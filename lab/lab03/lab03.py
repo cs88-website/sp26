28a1821adb22a26d01d1779b9920cff9d9e04a12
mira wagner <mirawagner@berkeley.edu>
2026-02-09 19:19:47 +0000

    Deploy website Mon Feb  9 19:19:40 UTC 2026

---
+++ lab/lab03/lab03.py
@@ -0,0 +1,83 @@
+def close(s: list[int], k: int) -> int:
+    """Return how many elements of s are within k of their index.
+
+    >>> t = [6, 2, 4, 3, 5]
+    >>> close(t, 0)  # Only 3 is equal to its index
+    1
+    >>> close(t, 1)  # 2, 3, and 5 are within 1 of their index
+    3
+    >>> close(t, 2)  # 2, 3, 4, and 5 are all within 2 of their index
+    4
+    >>> close(list(range(10)), 0)
+    10
+    """
+    assert k >= 0
+    count = 0
+    for i in range(len(s)):  # Use a range to loop over indices
+        "*** YOUR CODE HERE ***"
+    return count
+
+
+
+def composite_identity(f, g):
+    """
+    Return a function with one parameter x that returns True if f(g(x)) is
+    equal to g(f(x)). You can assume the result of g(x) is a valid input for f
+    and vice versa.
+
+    >>> add_one = lambda x: x + 1        # adds one to x
+    >>> square = lambda x: x**2          # squares x [returns x^2]
+    >>> b1 = composite_identity(square, add_one)
+    >>> b1(0)                            # (0 + 1) ** 2 == 0 ** 2 + 1
+    True
+    >>> b1(4)                            # (4 + 1) ** 2 != 4 ** 2 + 1
+    False
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def sum_digits(y):
+    """Return the sum of the digits of non-negative integer y."""
+    total = 0
+    while y > 0:
+        total, y = total + y % 10, y // 10
+    return total
+
+def is_prime(n):
+    """Return whether positive integer n is prime."""
+    if n == 1:
+        return False
+    k = 2
+    while k < n:
+        if n % k == 0:
+            return False
+        k += 1
+    return True
+
+def count_cond(condition):
+    """Returns a function with one parameter N that counts all the numbers from
+    1 to n that satisfy the two-argument predicate function Condition, where
+    the first argument for condition is n and the second argument is the
+    number from 1 to n.
+
+    >>> count_fives = count_cond(lambda n, i: sum_digits(n * i) == 5)
+    >>> count_fives(10)   # 50 (10 * 5)
+    1
+    >>> count_fives(50)   # 50 (50 * 1), 500 (50 * 10), 1400 (50 * 28), 2300 (50 * 46)
+    4
+
+    >>> is_i_prime = lambda n, i: is_prime(i) # need to pass 2-argument function into count_cond
+    >>> count_primes = count_cond(is_i_prime)
+    >>> count_primes(2)    # 2
+    1
+    >>> count_primes(3)    # 2, 3
+    2
+    >>> count_primes(4)    # 2, 3
+    2
+    >>> count_primes(5)    # 2, 3, 5
+    3
+    >>> count_primes(20)   # 2, 3, 5, 7, 11, 13, 17, 19
+    8
+    """
+    "*** YOUR CODE HERE ***"
+
