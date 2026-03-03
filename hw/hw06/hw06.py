6747e87fd0fc48d0be57dfe342f902a397949165
mira wagner <mirawagner@berkeley.edu>
2026-03-03 17:03:47 +0000

    Deploy website Tue Mar  3 17:03:24 UTC 2026

---
+++ hw/hw06/hw06.py
@@ -0,0 +1,94 @@
+SOURCE_FILE = __file__
+
+
+def max_product(s):
+    """Return the maximum product of non-consecutive elements of s.
+
+    >>> max_product([10, 3, 1, 9, 2])   # 10 * 9
+    90
+    >>> max_product([5, 10, 5, 10, 5])  # 5 * 5 * 5
+    125
+    >>> max_product([])                 # The product of no numbers is 1
+    1
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def next_larger_coin(coin):
+    """Returns the next larger coin in order.
+    >>> next_larger_coin(1)
+    5
+    >>> next_larger_coin(5)
+    10
+    >>> next_larger_coin(10)
+    25
+    >>> next_larger_coin(2) # Other values return None
+    """
+    if coin == 1:
+        return 5
+    elif coin == 5:
+        return 10
+    elif coin == 10:
+        return 25
+
+def next_smaller_coin(coin):
+    """Returns the next smaller coin in order.
+    >>> next_smaller_coin(25)
+    10
+    >>> next_smaller_coin(10)
+    5
+    >>> next_smaller_coin(5)
+    1
+    >>> next_smaller_coin(2) # Other values return None
+    """
+    if coin == 25:
+        return 10
+    elif coin == 10:
+        return 5
+    elif coin == 5:
+        return 1
+
+def count_coins(total):
+    """Return the number of ways to make change using coins of value of 1, 5, 10, 25.
+    >>> count_coins(15)
+    6
+    >>> count_coins(10)
+    4
+    >>> count_coins(20)
+    9
+    >>> count_coins(100) # How many ways to make change for a dollar?
+    242
+    >>> count_coins(200)
+    1463
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(SOURCE_FILE, 'count_coins', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def mario_number(level):
+    """Return the number of ways that Mario can perform a sequence of steps
+    or jumps to reach the end of the level without ever landing in a Piranha
+    plant. Assume that every level begins and ends with a space.
+
+    >>> mario_number(' P P ')   # jump, jump
+    1
+    >>> mario_number(' P P  ')   # jump, jump, step
+    1
+    >>> mario_number('  P P ')  # step, jump, jump
+    1
+    >>> mario_number('   P P ') # step, step, jump, jump or jump, jump, jump
+    2
+    >>> mario_number(' P PP ')  # Mario cannot jump two plants
+    0
+    >>> mario_number('    ')    # step, jump ; jump, step ; step, step, step
+    3
+    >>> mario_number('    P    ')
+    9
+    >>> mario_number('   P    P P   P  P P    P     P ')
+    180
+    """
+    "*** YOUR CODE HERE ***"
+
