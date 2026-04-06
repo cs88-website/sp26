c8920a7f495f24ade7a53d431749c931d5f14e08
mira wagner <mirawagner@berkeley.edu>
2026-04-06 17:05:53 +0000

    Deploy website Mon Apr  6 17:05:42 UTC 2026

---
+++ hw/sol-hw07/hw07.py
@@ -0,0 +1,272 @@
+from __future__ import annotations
+
+
+# You do not need to understand what this function does. It is used for testing.
+def make_test_random():
+    """A deterministic random function that cycles between
+    [0.0, 0.1, 0.2, ..., 0.9] for testing purposes.
+
+    >>> random = make_test_random()
+    >>> random()
+    0.0
+    >>> random()
+    0.1
+    >>> random2 = make_test_random()
+    >>> random2()
+    0.0
+    """
+    rands = [x / 10 for x in range(10)]
+    def random():
+        rand = rands[0]
+        rands.append(rands.pop(0))
+        return rand
+    return random
+
+### Phase 1: The Player Class
+class Player:
+    """
+    >>> random = make_test_random()
+    >>> p1 = Player('Hill', random)
+    >>> p2 = Player('Don', random)
+    >>> p1.popularity
+    100
+    >>> p1.debate(p2)  # random() should return 0.0
+    >>> p1.popularity
+    150
+    >>> p2.popularity
+    100
+    >>> p2.votes
+    0
+    >>> p2.speech(p1)
+    >>> p2.votes
+    10
+    >>> p2.popularity
+    110
+    >>> p1.popularity
+    135
+    >>> p1.speech(p2)
+    >>> p1.votes
+    13
+    >>> p1.popularity
+    148
+    >>> p2.votes
+    10
+    >>> p2.popularity
+    99
+    >>> for _ in range(4):  # 0.1, 0.2, 0.3, 0.4
+    ...     p1.debate(p2)
+    >>> p2.debate(p1)
+    >>> p2.popularity
+    49
+    >>> p2.debate(p1)
+    >>> p2.popularity
+    0
+    """
+    def __init__(self, name, random_func):
+        self.name = name
+        self.votes = 0
+        self.popularity = 100
+        self.random_func = random_func
+
+    def debate(self, other):
+        prob = max(0.1, self.popularity / (self.popularity + other.popularity))
+        if self.random_func() < prob:
+            self.popularity += 50
+        else:
+            self.popularity = max(0, self.popularity - 50)
+
+    def speech(self, other):
+        self.votes += self.popularity // 10
+        self.popularity += self.popularity // 10
+        other.popularity = max(0, other.popularity - (other.popularity // 10))
+
+    def choose(self, other):
+        return self.speech
+
+
+### Phase 2: The Game Class
+class Game:
+    """
+    >>> random = make_test_random()
+    >>> p1, p2 = Player('Hill',random), Player('Don', random)
+    >>> g = Game(p1, p2)
+    >>> winner = g.play()
+    >>> p1 is winner
+    True
+    >>> # Additional correctness tests
+    >>> winner is g.winner()
+    True
+    >>> g.turn
+    10
+    >>> p1.votes = p2.votes
+    >>> print(g.winner())
+    None
+    """
+    def __init__(self, player1, player2):
+        self.p1 = player1
+        self.p2 = player2
+        self.turn = 0
+
+    def play(self):
+        while not self.game_over():
+            if self.turn % 2 == 0:
+                curr, other = self.p1, self.p2
+            else:
+                curr, other = self.p2, self.p1
+            curr.choose(other)(other)
+            self.turn += 1
+        return self.winner()
+
+    def game_over(self):
+        return max(self.p1.votes, self.p2.votes) >= 50 or self.turn >= 10
+
+    def winner(self):
+        if self.p1.votes > self.p2.votes:
+            return self.p1
+        elif self.p2.votes > self.p1.votes:
+            return self.p2
+        else:
+            return None
+
+
+### Phase 3: New Players
+class AggressivePlayer(Player):
+    """
+    >>> random = make_test_random()
+    >>> p1, p2 = AggressivePlayer('Don', random), Player('Hill', random)
+    >>> g = Game(p1, p2)
+    >>> winner = g.play()
+    >>> p1 is winner
+    True
+    >>> # Additional correctness tests
+    >>> p1.popularity = p2.popularity
+    >>> p1.choose(p2) == p1.debate
+    True
+    >>> p1.popularity += 1
+    >>> p1.choose(p2) == p1.debate
+    False
+    >>> p2.choose(p1) == p2.speech
+    True
+    """
+    def choose(self, other):
+        if self.popularity <= other.popularity:
+            return self.debate
+        else:
+            return self.speech
+
+class CautiousPlayer(Player):
+    """
+    >>> random = make_test_random()
+    >>> p1, p2 = CautiousPlayer('Hill', random), AggressivePlayer('Don', random)
+    >>> p1.popularity = 0
+    >>> p1.choose(p2) == p1.debate
+    True
+    >>> p1.popularity = 1
+    >>> p1.choose(p2) == p1.debate
+    False
+    >>> # Additional correctness tests
+    >>> p2.choose(p1) == p2.speech
+    True
+    """
+    def choose(self, other):
+        if self.popularity == 0:
+            return self.debate
+        else:
+            return self.speech
+
+
+class Account:
+    """An account has a balance and a holder.
+
+    >>> a = Account('John')
+    >>> a.deposit(10)
+    10
+    >>> a.balance
+    10
+    >>> a.interest
+    0.02
+    >>> a.time_to_retire(10.25)  # 10 -> 10.2 -> 10.404
+    2
+    >>> a.balance                # Calling time_to_retire method should not change the balance
+    10
+    >>> a.time_to_retire(11)     # 10 -> 10.2 -> ... -> 11.040808032
+    5
+    >>> a.time_to_retire(100)
+    117
+    """
+    max_withdrawal: int = 10
+    interest: float = 0.02
+
+    def __init__(self, account_holder: str):
+        self.balance = 0
+        self.holder = account_holder
+
+    def deposit(self, amount: int) -> int:
+        self.balance = self.balance + amount
+        return self.balance
+
+    def withdraw(self, amount: int) -> int | str:
+        if amount > self.balance:
+            return "Insufficient funds"
+        if amount > self.max_withdrawal:
+            return "Can't withdraw that amount"
+        self.balance = self.balance - amount
+        return self.balance
+
+    def time_to_retire(self, amount: float) -> int:
+        """Return the number of years until balance would grow to amount."""
+        assert self.balance > 0 and amount > 0 and self.interest > 0
+        future = self.balance
+        years = 0
+        while future < amount:
+            future += self.interest * future
+            years += 1
+        return years
+
+
+class FreeChecking(Account):
+    """A bank account that charges for withdrawals, but the first two are free!
+
+    >>> ch = FreeChecking('Jack')
+    >>> ch.balance = 20
+    >>> ch.withdraw(100)  # First one's free. Still counts as a free withdrawal even though it was unsuccessful
+    'Insufficient funds'
+    >>> ch.withdraw(3)    # Second withdrawal is also free
+    17
+    >>> ch.balance
+    17
+    >>> ch.withdraw(3)    # Now there is a fee because free_withdrawals is only 2
+    13
+    >>> ch.withdraw(3)
+    9
+    >>> ch2 = FreeChecking('John')
+    >>> ch2.balance = 10
+    >>> ch2.withdraw(3) # No fee
+    7
+    >>> ch.withdraw(3)  # ch still charges a fee
+    5
+    >>> ch.withdraw(5)  # Not enough to cover fee + withdraw
+    'Insufficient funds'
+    """
+    withdraw_fee: int = 1
+    free_withdrawals: int = 2
+
+    def __init__(self, account_holder: str):
+        super().__init__(account_holder)
+        self.withdrawals = 0
+
+    def withdraw(self, amount: int) -> int | str:
+        self.withdrawals += 1
+        fee = 0
+        if self.withdrawals > self.free_withdrawals:
+            fee = self.withdraw_fee
+        return super().withdraw(amount + fee)
+
+    # Alternative solution where you don't need to include init.
+    # Check out the video solution for more.
+    def withdraw(self, amount: int) -> int | str:
+        self.free_withdrawals -= 1
+        if self.free_withdrawals >= 0:
+            return super().withdraw(amount)
+        return super().withdraw(amount + self.withdraw_fee)
+
