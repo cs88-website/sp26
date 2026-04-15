c3c067b1d2e6af72c7931d6cc5145ff14630f53f
Michael Ball <michael@mball.co>
2026-04-15 13:55:46 -0700

    Deploy website Wed Apr 15 13:55:40 PDT 2026

--- assets/slides/21.py
+++ assets/slides/21.py
@@ -1,3 +1,53 @@
+# Iterators wrap up:
+class myrange:
+    def __init__(self, n):
+        self.i = 0
+        self.n = n
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.i < self.n:
+            current = self.i
+            self.i += 1
+            return current
+        else:
+            raise StopIteration()
+
+class CountIterator:
+    """Wraps an iterator and tracks how many times next() has been called successfully."""
+
+    def __init__(self, iterator):
+        self._it = iter(iterator)
+        self._count = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        val = next(self._it)
+        self._count += 1
+        return val
+
+    def num_calls(self):
+        """Return the number of items yielded so far."""
+        return self._count
+
+ci = CountIterator(range(5))
+# >>> for val in ci:
+# ...     print(f"val={val}, calls={ci.num_calls()}")
+# val=0, calls=1
+# val=1, calls=2
+# val=2, calls=3
+# val=3, calls=4
+# val=4, calls=5
+# >>> ci.num_calls()
+# 5
+# >>> next(ci)
+# StopIteration
+# >>> ci.num_calls()
+# 5
+
+
 from functools import reduce
 from operator import add
 
