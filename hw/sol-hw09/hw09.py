c8d86d6792567b814e2e8379f448f15b46a235aa
mira wagner <mirawagner@berkeley.edu>
2026-04-20 17:35:45 +0000

    Deploy website Mon Apr 20 17:35:19 UTC 2026

---
+++ hw/sol-hw09/hw09.py
@@ -0,0 +1,211 @@
+class Tree:
+    """A tree has a label and a list of branches.
+
+    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+    >>> t.label
+    3
+    >>> t.branches[0].label
+    2
+    >>> t.branches[1].is_leaf()
+    True
+    """
+    def __init__(self, label, branches=[]):
+        self.label = label
+        for branch in branches:
+            assert isinstance(branch, Tree)
+        self.branches = list(branches)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            branch_str = ''
+        return 'Tree({0}{1})'.format(repr(self.label), branch_str)
+
+    def __str__(self):
+        return '\n'.join(self.indented())
+
+    def indented(self):
+        lines = []
+        for b in self.branches:
+            for line in b.indented():
+                lines.append('  ' + line)
+        return [str(self.label)] + lines
+
+
+class Link:
+    """A linked list.
+
+    >>> s = Link(1)
+    >>> s.first
+    1
+    >>> s.rest is Link.empty
+    True
+    >>> s = Link(2, Link(3, Link(4)))
+    >>> s.first = 5
+    >>> s.rest.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s                                    # Displays the contents of repr(s)
+    Link(5, Link(6))
+    >>> s.rest = Link(7, Link(Link(8, Link(9))))
+    >>> s
+    Link(5, Link(7, Link(Link(8, Link(9)))))
+    >>> print(s)                             # Prints str(s)
+    (5 7 (8 9))
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '('
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + ')'
+
+
+def label_squarer(t):
+    """Mutates a Tree t by squaring all its elements.
+
+    >>> t = Tree(1, [Tree(3, [Tree(5)]), Tree(7)])
+    >>> label_squarer(t)
+    >>> t
+    Tree(1, [Tree(9, [Tree(25)]), Tree(49)])
+    """
+    t.label = t.label ** 2
+    for subtree in t.branches:
+        label_squarer(subtree)
+
+
+def add_d_leaves(t, v):
+    """Add d leaves containing v to each node at every depth d.
+
+    >>> t_one_to_four = Tree(1, [Tree(2), Tree(3, [Tree(4)])])
+    >>> print(t_one_to_four)
+    1
+      2
+      3
+        4
+    >>> add_d_leaves(t_one_to_four, 5)
+    >>> print(t_one_to_four)
+    1
+      2
+        5
+      3
+        4
+          5
+          5
+        5
+
+    >>> t0 = Tree(9)
+    >>> add_d_leaves(t0, 4)
+    >>> t0
+    Tree(9)
+    >>> t1 = Tree(1, [Tree(3)])
+    >>> add_d_leaves(t1, 4)
+    >>> t1
+    Tree(1, [Tree(3, [Tree(4)])])
+    >>> t2 = Tree(2, [Tree(5), Tree(6)])
+    >>> t3 = Tree(3, [t1, Tree(0), t2])
+    >>> print(t3)
+    3
+      1
+        3
+          4
+      0
+      2
+        5
+        6
+    >>> add_d_leaves(t3, 10)
+    >>> print(t3)
+    3
+      1
+        3
+          4
+            10
+            10
+            10
+          10
+          10
+        10
+      0
+        10
+      2
+        5
+          10
+          10
+        6
+          10
+          10
+        10
+    """
+    def add_leaves(t, d):
+        for b in t.branches:
+            add_leaves(b, d + 1)
+        t.branches.extend([Tree(v) for _ in range(d)])
+    add_leaves(t, 0)
+
+
+def partial_tree(s: Link, n: int):
+    """Return a balanced binary search tree of the first n elements of Link s,
+    along with the rest of s.
+
+    >>> s = Link(1, Link(2, Link(3, Link(4, Link(5)))))
+    >>> partial_tree(s, 3)
+    (Tree(2, [Tree(1), Tree(3)]), Link(4, Link(5)))
+    >>> t = Link(-2, Link(-1, Link(0, s)))
+    >>> tree, lnk = partial_tree(t, 7) # first element of tuple goes into tree, second goes into lnk
+    >>> tree
+    Tree(1, [Tree(-1, [Tree(-2), Tree(0)]), Tree(3, [Tree(2), Tree(4)])])
+    >>> lnk
+    Link(5)
+    """
+    if n == 1:
+        return (Tree(s.first), s.rest)
+    elif n == 2:
+        return (Tree(s.first, [Tree(s.rest.first)]), s.rest.rest)
+    else:
+        left_size = (n - 1) // 2
+        right_size = n - left_size - 1
+        left, rest = partial_tree(s, left_size)
+        label, rest = rest.first, rest.rest
+        right, rest = partial_tree(rest, right_size)
+        return Tree(label, [left, right]), rest
+
+def sequence_to_tree(s: Link):
+    """Return a balanced binary search tree containing the elements of sorted Link s.
+
+    Note: this implementation is complete, but the definition of partial_tree
+    above is not complete.
+
+    >>> sequence_to_tree(Link(1, Link(2, Link(3))))
+    Tree(2, [Tree(1), Tree(3)])
+    >>> elements = Link(1, Link(2, Link(3, Link(4, Link(5, Link(6, Link(7)))))))
+    >>> sequence_to_tree(elements)
+    Tree(4, [Tree(2, [Tree(1), Tree(3)]), Tree(6, [Tree(5), Tree(7)])])
+    """
+
+    def link_length(s: Link):
+        """Return the length of Linked List s."""
+        length = 0
+        while s is not Link.empty:
+            length += 1
+            s = s.rest
+        return length
+
+    return partial_tree(s, link_length(s))[0]
+
---
+++ lab/lab11/lab11.sql
@@ -0,0 +1,42 @@
+CREATE TABLE finals (hall TEXT, course TEXT);
+
+INSERT INTO finals VALUES
+('RSF', '61A'),
+('Wheeler', '61A'),
+('Pimentel', '61A'),
+('Li Ka Shing', '61A'),
+('Stanley', '61A'),
+('RSF', '61B'),
+('Wheeler', '61B'),
+('Morgan', '61B'),
+('Wheeler', '61C'),
+('Pimentel', '61C'),
+('Soda 310', '61C'),
+('Soda 306', '10'),
+('RSF', '70');
+
+CREATE TABLE sizes (room TEXT, seats INTEGER);
+
+INSERT INTO sizes VALUES
+('RSF', 900),
+('Wheeler', 700),
+('Pimentel', 500),
+('Li Ka Shing', 300),
+('Stanley', 300),
+('Morgan', 100),
+('Soda 306', 80),
+('Soda 310', 40),
+('Soda 320', 30);
+
+
+CREATE TABLE big AS
+  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+
+
+CREATE TABLE remaining AS
+  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+
+
+CREATE TABLE sharing AS
+  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+
